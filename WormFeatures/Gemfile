045c4d405d788dbf6c8cffcd6e8503dbaa675b34
Jeremy Stewart <jlstewart379@gmail.com>
2013-07-17 13:11:35 -0400

    fixing project structure

--- WormFeatures/Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-gem 'multi_json'
-gem 'frank-cucumber'
-gem 'rspec'
-gem 'furter'
--- WormFeatures/features/step_definitions/finding_steps.rb
+++
@@ -1,6 +0,0 @@
-Given(/^I am on the "([^"]*)"$/) do |page|
-  on(WormPage).active?
-end
-Then(/^I can see that the label "([^"]*)" displays the text "([^"]*)"$/) do |which, text|
-  on(WormPage).send("label_#{which}").should.eql?(text)
-end
